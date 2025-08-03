d2bcdce52b8962bd7cd811f859eaf4936fbe14ee
AtLaEhEaM <99670121+AtLaEhEaM@users.noreply.github.com>
2025-08-03 14:31:24 +0500

    bullets

--- gmtkJam25_project/Assets/Scripts/Enemies/Robot/Bullet.cs
+++ gmtkJam25_project/Assets/Scripts/Enemies/Robot/Bullet.cs
@@ -3,10 +3,26 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
+    public int damage = 4;
 
     void Update()
     {
         transform.position += transform.up * speed * Time.deltaTime;
         Destroy(gameObject, 5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        HealthBehaviour health = other.GetComponent<HealthBehaviour>();
+
+        if (health != null)
+        {
+            //EntityBehaviour source = GetComponent<EntityBehaviour>();
+            Vector3 hitPoint = other.ClosestPoint(transform.position);
+            DamageModel dmg = new(transform, hitPoint, damage);
+            health.TakeDamage(dmg);
+
+            Destroy(gameObject);
+        }
     }
 }
